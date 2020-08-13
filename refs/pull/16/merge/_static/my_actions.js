e68187c09a141e6c82440818c8633a3e97630e55
GitHub Action <action@github.com>
2020-08-13 23:03:43 +0000

    Update "refs/pull/16/merge"

--- refs/pull/16/merge/_static/my_actions.js
+++ refs/pull/16/merge/_static/my_actions.js
@@ -4,26 +4,24 @@ $(function() {
         var tocCaption = elem.innerText.split("\n")[0];
 
         var visibleCaption = sessionStorage.getItem("visibleCaption");
-        if (visibleCaption)
-        {
+        if (visibleCaption) {
             //var isVisible = $(this).children("ul").is(":visible");
             var isVisible = $(elem).children("ul").is(":visible");
             if (((!isVisible) && (visibleCaption === tocCaption)) ||
-                (isVisible && (visibleCaption !== tocCaption)))
-            {
-                $(elem).children("ul").toggle();
-            }
-        }
-    };
+                (isVisible && (visibleCaption !== tocCaption))) {
 
-    if (sessionStorage.getItem("visibleCaption"))
-    {
-        $(".sidebar-group").each(toggleCaptionVisibility);
+                $(elem).children("ul").toggle();
+        }
     }
+};
+
+if (sessionStorage.getItem("visibleCaption")) {
+    $(".sidebar-group").each(toggleCaptionVisibility);
+}
 
     // $(".sidebar-group .caption-text a").attr("href", "#");
     $(".sidebar-group .caption-text a").click(function(event) {
-       event.preventDefault();
+        event.preventDefault();
     });
 
     $(".sidebar-group").click(function() {
